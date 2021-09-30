76698ef3678e9c304feb24062ffe61be9c0641e6
enjou1224 <enjou1224@outlook.com>
2021-09-30 13:21:10 +0800

    decode还在写(decode is still being written.)

--- rtl/decode.v
+++ rtl/decode.v
@@ -1,18 +1,23 @@
-/* 
+/*
     -- ============================================================================
     -- FILE NAME	: decode.v
     -- DESCRIPTION : 指令集架构
     -- ----------------------------------------------------------------------------
     -- Revision  Date		  Coding_by	 Comment
-    -- 1.0.0	  2021/09/28  enjou		 初版
+    -- 1.0.0	  2021/09/29  enjou		 初版
     -- ============================================================================
 */
+`include "RVG.vh"
 
 module decode (
     input   [63:0]  i_PC_64,
     input   [31:0]  i_Inst_32,
-    input   [63:0]  i_GRFData1_64,
-    input   [63:0]  i_GRFData2_64,
+
+    // 通用寄存器连接       (General Register Connection)
+    output
+    input   [63:0]  i_GRFReadData1_64,
+    input   [63:0]  i_GRFReadData2_64,
     input   [63:0]  i_CSRData_64,
 );
+
 endmodule
